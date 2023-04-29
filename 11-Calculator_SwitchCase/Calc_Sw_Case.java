103248af47c5d1ebb142940491b4775f62271a94
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-04-29 19:09:20 +0300

    Homework: Calculator with switch case

---
+++ 11-Calculator_SwitchCase/Calc_Sw_Case.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Calc_Sw_Case {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("First number: ");
+        int num1 = input.nextInt();
+        System.out.print("Second number: ");
+        int num2 = input.nextInt();
+
+        System.out.println("1-Add\n2-Sub\n3-Mul\n4-Div");
+        System.out.print("Select transaction: ");
+        int selection = input.nextInt();1
+
+        switch(selection){
+            case 1: System.out.println("Addition: "+ (num1+num2));
+                break;
+            case 2: System.out.println("Subtraction: "+ (num1-num2));
+                break;
+            case 3: System.out.println("Multiplication: "+ (num1*num2));
+                break;
+            case 4: System.out.println("Division: "+ (num1/num2));  
+                break; 
+        }
+
+    }
+}
