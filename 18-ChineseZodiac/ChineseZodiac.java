ce14ca3cd250a2be48d71c2bd6cf8ca167bdb433
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-05-11 13:58:02 +0300

    Chinese Zodiac update (written english)

--- 18-ChineseZodiac/ChineseZodiac.java
+++ 18-ChineseZodiac/ChineseZodiac.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ChineseZodiac {
     public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    String [] zodiacs = {"Maymun","Horoz","Köpek","Domuz","Fare","Öküz","Kaplan","Tavşan","Ejderha","Yılan","At","Koyun"};
+    String [] zodiacs = {"Monkey","Rooster","Dog","Pig","Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Goat"};
     System.out.print("Enter year of birth: ");
     int yearOfBirth = input.nextInt();
     System.out.println("Your Chinese Zodiac: " + zodiacs[(yearOfBirth %12)]);
