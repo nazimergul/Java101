550912267b71b0f4863f1a459a4ea24015cf7dbe
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-05-28 14:16:16 +0300

    General arrangement

--- 00-javaPatikasi/Baslangic.java
+++
@@ -1,9 +0,0 @@
-public class Baslangic {
-    public static void main(String[] args) {
-        int t = 0;
-        int i = 0;
-        t += i++;
-        System.out.println(t);
-        System.out.println(i);
-    }
-}
