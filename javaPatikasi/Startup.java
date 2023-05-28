70eda0bc26044a932157ee6758155fc7660aba58
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-05-28 14:15:50 +0300

    General arrangement

---
+++ javaPatikasi/Startup.java
@@ -0,0 +1,9 @@
+public class Startup {
+    public static void main(String[] args) {
+        int t = 0;
+        int i = 0;
+        t += i++;
+        System.out.println(t);
+        System.out.println(i);
+    }
+}
