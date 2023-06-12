ac37ad1040cfa394063756981a64c14dc613bae8
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-06-12 22:07:17 +0300

    Mod3And4 added

--- 03-Döngüler/01-Practice/Mod3And4.java
+++ 03-Döngüler/01-Practice/Mod3And4.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Mod3And4 {
     public static void main(String[] args) {
-        int intNumber;
+        int intNumber,bufferNumber,counterNumber;
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number: ");
         intNumber = input.nextInt();
-
+        bufferNumber = 0;
+        counterNumber = 0;
         for (int i = 0; i <= intNumber; i++) {
-            if (i % 3 == 0 && i % 4 == 0) {
-                System.out.println(i);
+            if (i % 3 == 0 && i % 4 == 0 && i != 0) {
+                
+                bufferNumber += i;
+                counterNumber +=1;
             }
         }
+        System.out.println("Avarage: "+bufferNumber/counterNumber);
     }
 }
