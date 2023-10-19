97f913b4bf338846e9dc553a28fb9e3cab6d1648
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-10-20 00:50:15 +0300

    Fibonacci finder with recursive method

---
+++ 04-Metotlar/02-RecursiveFibo/RecursiveFibo.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class RecursiveFibo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Fibonacci serisi için eleman sayısı giriniz: ");
+        int n = input.nextInt();
+        System.out.println("Sonuç: "+fib(n));
+    }
+    static int fib(int n){
+        if(n == 0){
+        
+        return 0;
+        }else if(n == 1){
+        
+        return 1;
+        }else if(n > 1){
+        
+        return fib(n-1) + fib(n-2);
+        }
+        return 0;
+     }
+}
