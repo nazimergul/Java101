c139bc0ebb813ad5bc83f77ccd2055d79e69823e
Nazim Ergul <nazim.ergul.dev@gmail.com>
2023-10-20 00:51:31 +0300

    Fibonacci finder with recursive method

--- 04-Metotlar/02-RecursiveFibo/RecursiveFibo.java
+++ 04-Metotlar/02-RecursiveFibo/RecursiveFibo.java
@@ -7,14 +7,11 @@ public class RecursiveFibo {
         System.out.println("Sonuç: "+fib(n));
     }
     static int fib(int n){
-        if(n == 0){
-        
+        if(n == 0){        
         return 0;
-        }else if(n == 1){
-        
+        }else if(n == 1){        
         return 1;
-        }else if(n > 1){
-        
+        }else if(n > 1){        
         return fib(n-1) + fib(n-2);
         }
         return 0;
